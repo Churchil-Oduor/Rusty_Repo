538747b0fc2460214dbcbe154d1b326847020c63
Churchil-Oduor <churchilokechoduor@gmail.com>
2025-02-24 11:03:47 +0300

    updated operations

--- doubly_linked_lists/src/main.rs
+++ doubly_linked_lists/src/main.rs
@@ -7,8 +7,10 @@ fn main() {
     let mut node = Some(Box::new(Node::new("Churchil".to_string())));    
     Node::add_node(&mut node, "Hope".to_string());
     Node::add_node(&mut node, "Brandy".to_string());
-   /* Node::print_list(&mut node);
-
+    Node::print_list(&mut node);
+    Node::delete_node_at(&mut node, -2);
+    Node::print_list(&mut node);
+/*
     let count = Node::count_nodes(&node);
     println!("{}", count);
     Node::add_node_end(&mut node, "Okech".to_string());
--- doubly_linked_lists/src/utils/operations.rs
+++ doubly_linked_lists/src/utils/operations.rs
@@ -20,4 +20,57 @@ impl Node{
         new_node.next = head.take();
         *head = Some(new_node);
     }
+
+    pub fn print_list(head: &mut Option<Box<Node>>) {
+
+        let mut current = head.as_mut();
+
+        while let Some(n) = current {
+            print!(" {} ->", n.data);
+            current = n.next.as_mut();
+        }
+
+        println!(" NULL");
+    }
+
+    pub fn delete_node_at(head: &mut Option<Box<Node>>, position: i32) {
+        let num_nodes = Self::count_nodes(head);
+        let mut current = head.as_mut();
+        let mut count = 0;
+        let mut help_pos = position;
+
+
+        if help_pos < 0 {
+            help_pos += num_nodes as i32;
+        }
+
+        if help_pos == 0 {
+            if let Some(n) = current {
+                *head = n.next.take();
+                return;
+            }
+        }
+
+        while let Some(n) = current {
+            if count + 1 == help_pos {
+                n.next = n.next.take().and_then(|node| node.next);
+                return;
+            }
+            count += 1;
+            current = n.next.as_mut();
+        }
+    }
+
+    pub fn count_nodes(head:&mut Option<Box<Node>>) -> u32 {
+        let mut current = head.as_mut();
+        let mut count = 0;
+
+        while let Some(n) = current {
+            count += 1;
+            current = n.next.as_mut();
+        }
+
+        count
+
+    }
 }
